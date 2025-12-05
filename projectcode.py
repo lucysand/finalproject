fdfa73c8c1fe33edf2c33f94eab679a480aeba9b
Anna DeWitt <anna@Annas-MacBook-Air-9.local>
2025-12-05 13:30:04 -0500

    functions

--- projectcode.py
+++ projectcode.py
@@ -1,1 +1,26 @@
-print("hello")
+#Names: Anna DeWitt, Lucy Sanders, Aamir Kapasi
+
+
+
+def call_api(file): 
+    pass
+
+
+def create_database(data):
+    pass
+
+
+def calulate_tree_borough(db):
+    pass
+
+
+def create_graph(c1, c2, c3):
+    pass
+
+
+
+def create_file(data):
+    pass
+
+
+
