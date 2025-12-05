fc670e4f937eceb0452445327b92e7a393438566
Anna DeWitt <anna@Annas-MacBook-Air-9.local>
2025-12-05 13:59:40 -0500

    functions fr this time i hope

--- projectcode.py
+++ projectcode.py
@@ -1,26 +1,72 @@
 #Names: Anna DeWitt, Lucy Sanders, Aamir Kapasi
 
 
+import pyplot
 
-def call_api(file): 
+
+#INITAL SETUP 
+def create_tables():
+    """Create database tables."""
+    pass  # Implementation goes here
+
+
+#DATA FETCHING
+def fetch_health_batch(limit):
+    """Fetch a batch of health data."""
+    pass  # Implementation goes here
+
+def fetch_tree_batch(limit):
+    """Fetch a batch of tree data."""
+    pass  # Implementation goes here
+
+def fetch_demo_batch(limit):
+    """Fetch a batch of demographic data."""
+    pass  # Implementation goes here
+
+
+
+#INSERT DATA
+def insert_health(records):
+    """Insert health records into the database."""
+    pass  # Implementation goes here
+
+def insert_tree(records):
+    """Insert tree records into the database."""
+    pass  # Implementation goes here    
+
+def insert_demo(records):
+    """Insert demographic records into the database."""
+    pass  # Implementation goes here
+
+
+
+# ?
+def select_joined_data():
+    """Select joined data from health, tree, and demographic tables."""
+    pass  # Implementation goes here
+
+
+#CALCULATIONS (may need to create more functions (???) tbd i guess)
+def compute_calculations():
+    """Compute necessary calculations on the data."""
+    pass  # Implementation goes here
+
+
+#VISUALIZATIONS
+def plot_income_race():
     pass
 
-
-def create_database(data):
+def plot_tree_income():
     pass
 
-
-def calulate_tree_borough(db):
+def plot_health_tree():
     pass
 
-
-def create_graph(c1, c2, c3):
+def plot_tree_health_distr():
     pass
-
-
-
-def create_file(data):
-    pass
+ 
+def plot_income_vs_asthma():
+    pass 
 
 
 
